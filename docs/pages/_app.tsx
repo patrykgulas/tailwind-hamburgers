1cb84f80a6b6bc2d5cb258b8c07151a7b4dc7726
Patryk Gulas <patrykgulas@Glovousers-MacBook-Pro.local>
2023-01-20 20:24:23 +0100

    feat: add vercel analytics

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,6 +1,12 @@
 import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 export default MyApp;
